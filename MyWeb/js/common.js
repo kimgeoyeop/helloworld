d75da44cd0b57309c00798111ece5faabc71af94
gykim <kky0109995@naver.com>
2023-01-10 12:53:02 +0900

    1. 방문일시 추가

--- MyWeb/js/common.js
+++ MyWeb/js/common.js
@@ -32,6 +32,23 @@ function setCookie(name, value, options = {}) {
     document.cookie = updatedCookie;
 }
 
+function getFullDateTime() {
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+
+    var timeString = hours + ':' + minutes  + ':' + seconds;
+    var dateString = year + '-' + month  + '-' + day + " " + timeString;
+
+    return dateString;
+}
+
 let TODAY_INVITE_CNT = 0;
 
 $(document).ready(function(){
@@ -82,6 +99,7 @@ $(document).ready(function(){
             data: {                
                 "tmp"      : "test",
                 "date"     : todayStr,
+                "visit_tm" : getFullDateTime(),
                 "sheet"    : "today"
             },
             success: function(response){
